47d8e5e2642c6617f40c1cd61d336d14463f2c09
danya <danyakresichenko0709@gmail.com>
2023-11-10 19:59:52 +0300

    REQ

--- rksi_app/models.py
+++ rksi_app/models.py
@@ -11,7 +11,7 @@ class Students(models.Model):
     group = models.ForeignKey('Groups', on_delete=models.SET_NULL, related_name='group', verbose_name='Группа')
     group_lead = models.ForeignKey('Teacher', on_delete=models.SET_NULL , related_name='group_lead', verbose_name='Классный руководитель')
     edu_price = models.ForeignKey('EduPrice', on_delete=models.SET_NULL, related_name='edu_price', verbose_name='Форма финансирования обучения')
-    edu_form = models.CharField(max_length=255, verbose_name='Форма обучения')
+    edu_form = models.ForeignKey('EduForm', on_delete=models.SET_NULL, related_name='edu_form', verbose_name='Форма обучения')
     health = models.ForeignKey('Health', on_delete=models.SET_NULL, related_name='health_group', verbose_name='Группа здоровья')
     social = models.ForeignKey('Social', on_delete=models.SET_NULL, verbose_name='Социальный статус')
     photo = models.ImageField(upload_to='photos/%Y/%m/&d', null=True, verbose_name='Фото')
@@ -62,10 +62,17 @@ class Articles(models.Model):
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     is_published = models.BooleanField(default=True, verbose_name='Статус публикации')
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         verbose_name = 'Новости'
         verbose_name_plural = 'Новости'
         ordering = ['time_create',]
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(pytils_slugify(self.title))
+        super().save(*args, **kwargs)
 
 class Photo(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', null=True, verbose_name='Фото')
